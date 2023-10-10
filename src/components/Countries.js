8d0c75521dda40431d8b36a8a78e4b49f1c8630d
LPF-Quentin <quentin-rousseau@orange.fr>
2023-10-10 11:50:30 +0200

    a

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Countries = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([])
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
 
-    useEffect(() => {
-        axios
-            .get('https://restcountries.com/v3.1/all')
-            .then((res) => setData(res.data));
-    }, []);
-
-    return (
-        <div className='countries'>
-            <h1>az</h1>
-            <ul>
-                {
-                    data.map((country, index) =>(<li key={index}>{country.translations.fra.common}</li>)
-                )}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="countries">
+      <h1>az</h1>
+      <ul>
+        {data.map((country) => (
+          <li className="card">
+            <img src={country.flags.svg} alt="img" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Countries;
