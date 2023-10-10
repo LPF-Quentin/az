fc1f40b23f4ea9f69f82b40d99f18088cacd839c
LPF-Quentin <98741442+LPF-Quentin@users.noreply.github.com>
2023-10-10 09:22:15 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from './../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>a propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum voluptatum ipsam dolor alias sint 
+                perspiciatis optio et! Non suscipit impedit sit, culpa aliquam totam deleniti dolorem tempora sequi 
+                ex ad atque, aperiam voluptatibus nulla quas nisi delectus, asperiores molestias. Deleniti, nostrum! 
+                Eaque saepe dolores dolorem quia numquam nisi debitis animi itaque facilis! Reiciendis temporibus facilis, 
+                error modi culpa eos dolorem adipisci asperiores dolor labore. Est modi quod iste, asperiores repellendus ea, 
+                quia placeat et ducimus magni at natus expedita praesentium. Consequatur alias debitis aliquid, inventore 
+                adipisci aut doloribus, magni tempore quae sed modi cum minima explicabo impedit voluptatibus ab dicta!
+            </p>
+        </div>
+    );
+};
+
+export default About;
